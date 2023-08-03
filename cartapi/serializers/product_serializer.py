58bee2c797280e8306b249f2fec230dfc6976470
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-08-03 11:07:23 -0500

    all endpoints working

--- cartapi/serializers/product_serializer.py
+++ cartapi/serializers/product_serializer.py
@@ -10,7 +10,6 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id',
-                  'category', 
                   'seller',
                   'name',
                   'price',
--- cartapi/views/order_item_view.py
+++ cartapi/views/order_item_view.py
@@ -8,8 +8,39 @@ class OrderItemView(ViewSet):
     def create(self, request):
         """POST request for order_item or 'cart item' for user's cart
         Two conditions for this method: a user already has an open order, or a user does not have an open order and one must be created"""
+        user = User.objects.get(pk=request.META['HTTP_AUTHORIZATION'])
+        product = Product.objects.get(pk=request.data["productId"])
+        order_item = {
+            'product': product
+        }
+        try:
+            order = Order.objects.get(is_open=True, buyer=user)
+            order_item['order'] = order
+        except Order.DoesNotExist:
+            new_order = Order(buyer=user)
+            new_order.save()
+            order_item['order'] = new_order
+
+        OrderItem.objects.create(
+            order = order_item['order'],
+            product = order_item['product']
+        )
+
+        return Response({'message': f'{product.name} added to cart!'}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk):
         """DELETE request to remove item from cart
         handles two conditions: removing a single item from cart, and removing all instances of product from cart
         pk=product id"""
+        user = User.objects.get(pk=request.META['HTTP_AUTHORIZATION'])
+        order = Order.objects.get(buyer=user, is_open=True)
+        product = Product.objects.get(pk=pk)
+        order_items = OrderItem.objects.filter(product=product, order=order)
+        try:
+            request.META['HTTP_DECREMENT']
+            order_items[0].delete()
+            return Response({'message': f'1 {product.name} removed from cart!'}, status=status.HTTP_204_NO_CONTENT)
+        except KeyError:
+            for item in order_items:
+                item.delete()
+        return Response({'message': f'{product.name} removed from cart!'}, status=status.HTTP_204_NO_CONTENT)
